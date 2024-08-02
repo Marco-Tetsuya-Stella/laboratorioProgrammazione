96b52f2cd06571db5695219ad67c0f336a2b842c
marco <marco.stella@edu.unifi.it>
2024-08-03 00:02:52 +0200

    created class Date

---
+++ Date.h
@@ -0,0 +1,19 @@
+//
+// Created by marco on 03/08/24.
+//
+
+#ifndef ELABORATO_LABORATORIO_DI_PROGRAMMAZIONE_DATE_H
+#define ELABORATO_LABORATORIO_DI_PROGRAMMAZIONE_DATE_H
+
+
+class Date {
+
+
+private:
+    int year=0;
+    int month=0;
+    int day=0;
+};
+
+
+#endif //ELABORATO_LABORATORIO_DI_PROGRAMMAZIONE_DATE_H
